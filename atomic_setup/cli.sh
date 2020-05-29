084d5f972870f6dc2dd3e93146f13bfed74d04a8
Kong <zkong@gsma.com>
2020-05-29 09:37:31 +0000

    Feature Update - configure TLS client authentication on Orderer - added client auth key + certs - update script to deploy peer with TLS client authentication - update cli.sh to use TLS client authentication with peer. - update cli.sh to auto populate command parameters using ORDAUTH, eg ./cli.sh XXX ORDAUTH, will replace with '--tls --cafile   --clientauth --certfile  --keyfile '

--- atomic_setup/cli.sh
+++ atomic_setup/cli.sh
@@ -2,16 +2,24 @@
 . setup.sh
 
 export PATH=${BASE}bin:${BASE}:$PATH
-export ORDERER_CA=${BASE}peer/organizations/ordererOrganizations/hldid.org/orderers/orderer.hldid.org/msp/tlscacerts/tlsca.hldid.org-cert.pem
+
+export ORDERER_CA=$(pwd)/tlsca.orderer.hldid.org-cert.pem
+export ORDERER_MTLS_CRT=$(pwd)/mtls.orderer.hldid.org-cert.crt
+export ORDERER_MTLS_KEY=$(pwd)/mtls.orderer.hldid.org-key.key
 
 #export FABRIC_LOGGING_SPEC=DEBUG
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID=${ORG}MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=${PV_PATH}${MYHOST}-pv-volume/peer/peers/peer0.${HOSTNAME}.${DOMAIN}/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PV_PATH}${MYHOST}-pv-volume/peer/users/Admin@${HOSTNAME}.${DOMAIN}/msp
+export CORE_PEER_TLS_CLIENTAUTHREQUIRED=true
+export CORE_PEER_TLS_CLIENTCERT_FILE=${PV_PATH}${MYHOST}-pv-volume/peer/users/Admin@${HOSTNAME}.${DOMAIN}/msp/signcerts/Admin@${HOSTNAME}.${DOMAIN}-cert.pem
+export CORE_PEER_TLS_CLIENTKEY_FILE=${PV_PATH}${MYHOST}-pv-volume/peer/users/Admin@${HOSTNAME}.${DOMAIN}/msp/keystore/priv_sk
 export CORE_PEER_ADDRESS=peer0.${HOSTNAME}.${DOMAIN}:${PORT}
 export FABRIC_CFG_PATH=template
 
+
+AUTH="--tls --cafile $ORDERER_CA  --clientauth --certfile $ORDERER_MTLS_CRT --keyfile $ORDERER_MTLS_KEY"
 CMD=$@
 
 echo "Setting ENV Variables"
@@ -19,7 +27,7 @@ env | grep CORE
 env | grep ORDERER
 echo 
 
-CMD2=$(echo "${CMD}"|awk '//{gsub("{", "'"'"'{"); gsub("}", "}'"'"'"); gsub("\\^", "\"");  print}')
+CMD2=$(echo "${CMD}"|awk '//{gsub("{", "'"'"'{"); gsub("}", "}'"'"'"); gsub("\\^", "\""); gsub("ORDAUTH", "${AUTH}");  print}')
 set -x
 eval "${CMD2}"
 set +x
