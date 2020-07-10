d28f11697e3ea03d834cbc90ac83b455f52996e5
Simon Schulz <simon.schulz01@telekom.de>
2020-07-10 10:53:55 +0200

    use correct chaincode

--- atomic_setup/scripts/deploy_counter_chaincode.sh
+++ atomic_setup/scripts/deploy_counter_chaincode.sh
@@ -6,12 +6,17 @@ set -e -o pipefail
 echo "> setting namespace"
 kubectl config set-context --current --namespace=$CFG_KUBENS
 
-
-echo "> copying counter.tgz to pod"
-kubectl cp chaincode/counter/javascript fabric-tools:/opt/counter_chaincode
-
-echo "> rebuilding chaincode "
-kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode package counter.tar.gz --path /opt/counter_chaincode --lang node --label counter_v1
+USE_PREPACKED_CHAINCODE=true
+if [ "$USE_PREPACKED_CHAINCODE" == "true" ]; then
+    echo "> using prepacked chaincode, copying counter.tar.gz to pod"
+    kubectl cp chaincode/counter/pkg/counter.tar.gz fabric-tools:/opt
+else
+    echo "> copying counter package to pod"
+    kubectl cp chaincode/counter/javascript fabric-tools:/opt/counter_chaincode
+    
+    echo "> rebuilding chaincode "
+    kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode package /opt/counter.tar.gz --path /opt/counter_chaincode --lang node --label counter_v1
+fi
 
 echo "> installing chaincode "
-kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode install counter.tar.gz
+kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode install /opt/counter.tar.gz
