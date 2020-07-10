dfce55f62b8cbe921d5f9058eaf165252c3973a1
Simon Schulz <simon.schulz01@telekom.de>
2020-07-10 11:08:31 +0200

    build from source works

--- atomic_setup/scripts/deploy_counter_chaincode.sh
+++ atomic_setup/scripts/deploy_counter_chaincode.sh
@@ -6,16 +6,16 @@ set -e -o pipefail
 echo "> setting namespace"
 kubectl config set-context --current --namespace=$CFG_KUBENS
 
-USE_PREPACKED_CHAINCODE=true
+USE_PREPACKED_CHAINCODE=false
 if [ "$USE_PREPACKED_CHAINCODE" == "true" ]; then
     echo "> using prepacked chaincode, copying counter.tar.gz to pod"
     kubectl cp chaincode/counter/pkg/counter.tar.gz fabric-tools:/opt
 else
     echo "> copying counter package to pod"
-    kubectl cp chaincode/counter/javascript fabric-tools:/opt/counter_chaincode
+    kubectl cp chaincode fabric-tools:chaincode
     
     echo "> rebuilding chaincode "
-    kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode package /opt/counter.tar.gz --path /opt/counter_chaincode --lang node --label counter_v1
+    kubectl exec fabric-tools -- /opt/remote_cli.sh peer lifecycle chaincode package /opt/counter.tar.gz --path chaincode/counter/javascript/ --lang node --label counter_v1
 fi
 
 echo "> installing chaincode "
