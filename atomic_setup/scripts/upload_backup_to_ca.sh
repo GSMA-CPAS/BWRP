8f497a9a9a3ab8c1dc77232ad73a13a23e072f8e
Simon Schulz <simon.schulz01@telekom.de>
2020-07-15 16:39:46 +0200

    fix deployment dir on ca backup restore

--- atomic_setup/scripts/upload_backup_to_ca.sh
+++ atomic_setup/scripts/upload_backup_to_ca.sh
@@ -6,5 +6,6 @@ set -e -o pipefail
 . setup.cfg
 
 echo "> uploading CA config backup to pod..."
+kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR
 kubectl cp $CFG_CONFIG_PATH_PVC/ca fabric-ca-tools:$CFG_PEER_DIR
 
