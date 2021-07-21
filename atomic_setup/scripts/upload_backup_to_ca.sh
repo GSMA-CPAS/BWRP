b28573c8f1943e0ad26ecf83183250e122d2cca5
Jörn Hoos <joern.hoos@telekom.de>
2021-07-21 16:31:07 +0200

    Selective deletion of files during CA deployment

--- atomic_setup/scripts/upload_backup_to_ca.sh
+++ atomic_setup/scripts/upload_backup_to_ca.sh
@@ -6,6 +6,14 @@ set -e -o pipefail
 . setup.cfg
 
 echo "> uploading CA config backup to pod..."
-kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR
+#kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR
+kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR/home
+kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR/peers
+kubectl exec fabric-ca-tools -- rm -rf $CFG_PEER_DIR/users
+kubectl exec fabric-ca-tools -- rm -f *.json
+kubectl exec fabric-ca-tools -- rm -f *.yaml
+
+
+
 kubectl cp $CFG_CONFIG_PATH_PVC/ca fabric-ca-tools:$CFG_PEER_DIR
 
