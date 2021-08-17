4c96946c73b97949d7ddbf4dc7e5870152088719
Bozhidar Ignatov <ignatov@cst-bg.net>
2021-08-17 15:58:32 +0300

    update_blockchain_adapter.sh with stop of BA

--- atomic_setup/scripts/update_blockchain_adapter.sh
+++ atomic_setup/scripts/update_blockchain_adapter.sh
@@ -8,6 +8,9 @@ set -e -o pipefail
 echo "> setting namespace"
 kubectl config set-context --current --namespace=$CFG_KUBENS
 
+echo "> stopping blockchain-adapter pod"
+kubectl delete -f $CFG_CONFIG_PATH/kubernetes/blockchain-adapter-pod.yaml
+
 echo "> upload setup_offchain_endpoint.sh and setup_sign_certs.sh"
 kubectl cp $CFG_CONFIG_PATH/scripts/setup_offchain_endpoint.sh fabric-tools:/opt/
 kubectl cp $CFG_CONFIG_PATH/scripts/setup_sign_certs.sh fabric-tools:/opt/
@@ -15,7 +18,7 @@ kubectl cp $CFG_CONFIG_PATH/scripts/setup_sign_certs.sh fabric-tools:/opt/
 echo "> upload ccp"
 kubectl -n $CFG_KUBENS cp $CFG_CONFIG_PATH/config/ccp/${CFG_ORG}.json fabric-tools:/mnt/data/CCP/${CFG_ORG}.json
 
-echo "> deploying blockchain-adapter pod and svc"
+echo "> deploying blockchain-adapter pod"
 kubectl apply -f $CFG_CONFIG_PATH/kubernetes/blockchain-adapter-pod.yaml
 
 echo "> waiting for blockchain-adapter pod to be ready"
